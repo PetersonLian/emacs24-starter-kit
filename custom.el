047ab23eae1e5494a6cc723adb9716c39ce0d371
Peterson Lian <lianpeipei202@gmail.com>
2014-12-25 10:59:11 +0800

    2014-12-25 11:00

--- custom.el
+++ custom.el
@@ -6,11 +6,10 @@
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (deeper-blue)))
- '(show-paren-mode t)
- '(tool-bar-mode nil)
  '(org-agenda-files (quote ("~/Dropbox/org")))
- '(org-stuck-projects (quote("+LEVEL=1-TODO=\"DONE\"-TODO=\"CANCELED\"" ("TODO" "NEXT" "NEXTACTION" "FEEDBACK" "CIRCLELY") nil "")))
-)
+ '(org-stuck-projects (quote ("+LEVEL=1-TODO=\"DONE\"-TODO=\"CANCELED\"-TODO=\"REVIEW\"" ("TODO" "NEXT" "NEXTACTION" "FEEDBACK" "CIRCLELY") nil "")))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
